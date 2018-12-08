161727b43b3993ec63c2ec41d5972afef99899d1
Milos Lajic <milos.laja.lajic@gmail.com>
2018-12-08 18:03:52 +0100

    Mitosis system wip. Avoidance system wip.

---
+++ Assets/Scripts/AvoidanceSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class AvoidanceSystem : JobComponentSystem
+{
+    
+    private struct PositionJob : IJobProcessComponentData<AvoidanceData, Position>
+    {
+        public void Execute(ref AvoidanceData data, ref Position position)
+        {
+            
+        }
+        
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new PositionJob();
+        return job.Schedule(this, inputDeps);
+    }
+}
+
+public struct AvoidanceData : IComponentData
+{
+    public float2 Force;
+}
