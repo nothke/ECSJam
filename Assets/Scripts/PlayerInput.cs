01b96638a36d61910e99739aaacd105fa3ea22c3
Gladleyfucker <jasatomicanin@yahoo.com>
2018-12-09 07:49:31 +0100

    Inputs for joypad.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public float horizontalAxis;
+    public float verticalAxis;
+    public float rHorizontalAxis;
+    public float rVerticalAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+        rHorizontalAxis = Input.GetAxis("rHorizontal");
+        rVerticalAxis = Input.GetAxis("rVertical");
+
+        if (horizontalAxis != 0 || verticalAxis != 0) {
+            //Do
+            print(horizontalAxis + " vert " + verticalAxis);
+        }
+
+        if (rHorizontalAxis != 0 || rVerticalAxis != 0)
+        {
+            //Do 
+            print(horizontalAxis + " vert " + verticalAxis);
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
+            // button
+            print("button");
+        }
+
+    }
+}
